f015a54aa59444b442d5d235771ad3e01e3c5f54
Bella3009 <bellasara13@gmail.com>
2023-05-19 23:56:41 +0200

    Working on the result of the word. Still need to show it in the website but the definition is correctly retrieved from csv file.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+data = pd.read_csv("dictionary.csv")
 @app.route("/")
 def home():
-    return render_template("Dictionery_index.html")
+    return render_template("index.html")
 
 @app.route("/api/v1/<word>")
 def dic_word(word):
+    word = word.lower()
+    define = data.loc[data["words"] == word]["definition"]
+    print(define)
     
     return {"Word": word,
-                 "Definition": word.upper()}
+            "Definition": 0}
 if __name__ == "__main__":
     app.run(debug=True)
