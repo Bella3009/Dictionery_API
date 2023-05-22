ffbaf0c067f797a1470a9da40e0751091a3e0163
Bella3009 <bellasara13@gmail.com>
2023-05-22 21:12:59 +0200

    The website correctly shown on the website.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 app = Flask(__name__)
 
 data = pd.read_csv("dictionary.csv")
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -11,10 +12,10 @@ def home():
 @app.route("/api/v1/<word>")
 def dic_word(word):
     word = word.lower()
-    define = data.loc[data["words"] == word]["definition"]
-    print(define)
-    
+    resDict = data.loc[data["words"] == word]["definition"].squeeze()
+    print(resDict)
     return {"Word": word,
-            "Definition": 0}
+            "Definition": resDict}
+
 if __name__ == "__main__":
     app.run(debug=True)
